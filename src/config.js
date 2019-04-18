0679316f4cba733f905e5ffa6c5eaec402bd7d4d
Alex <Zizzs17@gmail.com>
2019-04-18 12:59:20 -0700

    Fixed some errors

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
-const botToken = "NTY4MDg5ODU0MDA5OTMzODM0.XLdCBw.unwpA--ZOoctJsRC1N8QAH8TyAY";
-const url =
-  "mongodb+srv://bot:epicodus@epicodusdiscordbot-wqzjd.mongodb.net/test?retryWrites=true";
+const botToken = "XXXX";
+const url = "XXXX";
 const dbName = "DiscordBot";
 
 module.exports.botToken = botToken;
